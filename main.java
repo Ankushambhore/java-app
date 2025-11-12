d438c9a8144e88879be46fbc02f05fc226bc11b1
ankush <ankey012@gmail.com>
2025-11-12 12:27:15 +0000

    update12

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcomeggg to Java Application!");
+        System.out.println("Welcomeggg urvyyyy to Java Application!");
     }
 }
